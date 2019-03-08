29e9dab626ff90a8323d1f865166d0ca71062688
AWSafran <andrewsafran@gmail.com>
2019-03-08 12:57:08 -0500

    added infinite loop carousel function

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
 class Carousel {
+    constructor(input){
+        this.element = input;
+        this.leftButton = this.element.querySelector(".left-button");
+        this.rightButton = this.element.querySelector(".right-button");
+        console.log(this.element);
+        console.log(this.rightButton);
+        console.log(this.leftButton);
+        this.count = 0;
+        this.images = this.element.querySelectorAll("img");
+        this.images[0].style.display = "block";
 
+        this.leftButton.addEventListener('click', () => this.moveLeft());
+        this.rightButton.addEventListener('click', () => this.moveRight());
+    }
+
+    moveLeft(){
+        this.images[this.count].style.display = "none";
+        if (this.count === 0){
+            this.count = 3;
+        }
+        else{
+            --this.count;
+        }
+        this.images[this.count].style.display = "block";
+    }
+
+    moveRight(){
+        this.images[this.count].style.display = "none";
+        if (this.count === 3){
+            this.count = 0;
+        }
+        else{
+            ++this.count;
+        }
+        this.images[this.count].style.display = "block";
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+
+let carouselObject = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
